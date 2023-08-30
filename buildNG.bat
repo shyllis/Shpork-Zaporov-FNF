5ecebeea6dc55f213bdc604011420d816e292153
Shyllis <dolpshyosu@gmail.com>
2023-08-30 17:47:53 -0600

    ng building

---
+++ buildNG.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0a
+@echo on
+echo BUILDING GAME
+lime build html5 -final -Dng
+echo ZIPPING
+cd export
+7z a -tzip -r swag release\html5\bin
+start .
+start https://www.newgrounds.com/projects/games/1528775
+pause
